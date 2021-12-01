e6696655dce6e4c00c1bf8d46755d9d8f7c17464
Tachikoma000 <89101179+Tachikoma000@users.noreply.github.com>
2021-12-01 15:50:28 -0600

    updated dash hmlt and core component import syntax

--- apps/playgroundsSimulation_KlimaBonding.py
+++ apps/playgroundsSimulation_KlimaBonding.py
@@ -1,7 +1,7 @@
 # Import all required packages for this page
-import dash_core_components as dcc
+from dash import dcc
 import dash_bootstrap_components as dbc
-import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
