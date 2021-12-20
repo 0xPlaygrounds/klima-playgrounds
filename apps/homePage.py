c043fa3860651fd090ac39398dc250006700a5e3
Tachikoma000 <89101179+Tachikoma000@users.noreply.github.com>
2021-12-20 15:04:17 -0600

    bonding page ui/ux revamp

--- apps/homePage.py
+++ apps/homePage.py
@@ -3,7 +3,6 @@ import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 from dash import html
 # import dash_extensions as de  # pip install dash-extensions
 
-from components.disclaimer import short_disclaimer_row
 
 url = "https://assets6.lottiefiles.com/packages/lf20_0ac4xdrp.json"
 options = dict(loop=True, autoplay=True, rendererSettings=dict(preserveAspectRatio='xMidYMid slice'))
