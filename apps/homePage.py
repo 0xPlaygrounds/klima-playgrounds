bcaa2882243131cb69cec13a998a15280032659f
Tachikoma000 <89101179+Tachikoma000@users.noreply.github.com>
2022-02-02 19:56:38 -0600

    Updates to import lib

--- apps/homePage.py
+++ apps/homePage.py
@@ -1,9 +1,6 @@
 import dash  # pip install dash
 import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 from dash import html
-from app import app
-from apps import playgroundSimulation_KlimaGrowthOverTime, \
-                 playgroundsSimulation_KlimaBonding, quizzes_experimental, disclaimerPage, homePage
 
 url = "https://assets6.lottiefiles.com/packages/lf20_0ac4xdrp.json"
 options = dict(loop=True, autoplay=True, rendererSettings=dict(preserveAspectRatio='xMidYMid slice'))
