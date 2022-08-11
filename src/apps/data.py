e1af88f351a3346e56ccbf6d7449255db2d55b7e
Sekula <sekuloskimilos@gmail.com>
2022-08-11 14:26:42 +0200

    treasury-asset-breakdown: - Added treasury asset breakdown chart in USD (Including USDC and LP) - Added carbon custodied chart - Added DAO wallet balance chart (For USDC and KLIMA)

--- src/apps/data.py
+++ src/apps/data.py
@@ -1,5 +1,8 @@
 from subgrounds.dash_wrappers import AutoUpdate
 from subgrounds.plotly_wrappers import Figure, Scatter
+
+from src.apps.subgraph_data.treasury_assets import MODE_ASSET_VALUE_IN_USD, MODE_CARBON_CUSTODIED, TreasuryAssetWrapper
+
 from ..klima_subgrounds import sg, protocol_metrics_1year, get_klima_breakdown_df
 from ..app import app
 import time
@@ -511,6 +514,114 @@ def klima_alloc():
                       'spikemode': 'across', 'spikethickness': 0.5},
             'yaxis': {'type': 'linear', 'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white',
                       'title': 'KLIMA(%)', 'showgrid': False, 'mirror': True,
+                      'showspikes': True, 'spikesnap': 'cursor',
+                      'spikemode': 'across', 'spikethickness': 0.5},
+            'legend.font.color': 'white',
+            'paper_bgcolor': 'rgba(0,0,0,0)',
+            'plot_bgcolor': 'rgba(0,0,0,0)',
+            'autosize': True,
+            'margin': dict(l=20, r=30, t=10, b=20),
+            'legend': dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+            'modebar_add': ['drawline',
+                            'drawopenpath',
+                            'drawclosedpath',
+                            'drawcircle',
+                            'drawrect',
+                            'eraseshape'
+                            ],
+        }
+    ).figure
+
+# @wrap_autoupdate(seconds=21600)
+def treasury_value_in_usd():
+    treasury_asset_wrapper = TreasuryAssetWrapper(MODE_ASSET_VALUE_IN_USD)
+    return Figure(
+        subgrounds=sg,
+        traces=treasury_asset_wrapper.create_scatters(),
+        layout={
+            'showlegend': True,
+            'xaxis': {'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white', 'showgrid': False, 'mirror': True,
+                      'showspikes': True, 'spikesnap': 'cursor',
+                      'spikemode': 'across', 'spikethickness': 0.5},
+            'yaxis': {'type': 'linear', 'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white',
+                      'title': 'Market value (Milions)', 'showgrid': False, 'mirror': True,
+                      'showspikes': True, 'spikesnap': 'cursor',
+                      'spikemode': 'across', 'spikethickness': 0.5},
+            'legend.font.color': 'white',
+            'paper_bgcolor': 'rgba(0,0,0,0)',
+            'plot_bgcolor': 'rgba(0,0,0,0)',
+            'autosize': True,
+            'margin': dict(l=20, r=30, t=10, b=20),
+            'legend': dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+            'modebar_add': ['drawline',
+                            'drawopenpath',
+                            'drawclosedpath',
+                            'drawcircle',
+                            'drawrect',
+                            'eraseshape'
+                            ],
+        }
+    ).figure
+
+# @wrap_autoupdate(seconds=21600)
+def treasury_carbon_custodied():
+    treasury_asset_wrapper = TreasuryAssetWrapper(MODE_CARBON_CUSTODIED)
+    return Figure(
+        subgrounds=sg,
+        traces=treasury_asset_wrapper.create_scatters(),
+        layout={
+            'showlegend': True,
+            'xaxis': {'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white', 'showgrid': False, 'mirror': True,
+                      'showspikes': True, 'spikesnap': 'cursor',
+                      'spikemode': 'across', 'spikethickness': 0.5},
+            'yaxis': {'type': 'linear', 'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white',
+                      'title': 'Carbon custodied in tonnes', 'showgrid': False, 'mirror': True,
+                      'showspikes': True, 'spikesnap': 'cursor',
+                      'spikemode': 'across', 'spikethickness': 0.5},
+            'legend.font.color': 'white',
+            'paper_bgcolor': 'rgba(0,0,0,0)',
+            'plot_bgcolor': 'rgba(0,0,0,0)',
+            'autosize': True,
+            'margin': dict(l=20, r=30, t=10, b=20),
+            'legend': dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+            'modebar_add': ['drawline',
+                            'drawopenpath',
+                            'drawclosedpath',
+                            'drawcircle',
+                            'drawrect',
+                            'eraseshape'
+                            ],
+        }
+    ).figure
+
+# @wrap_autoupdate(seconds=21600)
+def dao_wallet_balances():
+    return Figure(
+        subgrounds=sg,
+        traces=[
+            Scatter(
+                name='KLIMA',
+                x=protocol_metrics_1year.datetime,
+                y=protocol_metrics_1year.daoBalanceKLIMA,
+                line={'width': 0.5, 'color': 'rgb(255, 0, 255)'},
+                stackgroup='one',
+            ),
+            Scatter(
+                name='USDC',
+                x=protocol_metrics_1year.datetime,
+                y=protocol_metrics_1year.daoBalanceUSDC,
+                line={'width': 0.5, 'color': 'rgb(2, 193, 50)'},
+                stackgroup='one',
+            ),
+        ],
+        layout={
+            'showlegend': True,
+            'yaxis': {'type': 'linear', 'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white',
+                      'title': 'Amount', 'showgrid': False, 'mirror': True,
+                      'showspikes': True, 'spikesnap': 'cursor',
+                      'spikemode': 'across', 'spikethickness': 0.5},
+            'xaxis': {'linewidth': 0.1, 'linecolor': '#31333F', 'color': 'white',
+                      'showgrid': False, 'mirror': True,
                       'showspikes': True, 'spikesnap': 'cursor',
                       'spikemode': 'across', 'spikethickness': 0.5},
             'legend.font.color': 'white',
---
+++ src/apps/subgraph_data/treasury_assets.py
@@ -0,0 +1,83 @@
+from src.apps.subgraph_data.protocol_metrics import get_bct_treasury, get_df_for_treasury_asset_by_address
+
+from src.apps.util.constants import BCT_ERC20_CONTRACT, BCT_USDC_PAIR, \
+     KLIMA_BCT_PAIR, KLIMA_MCO2_PAIR, KLIMA_USDC_PAIR, MCO2_ERC20_CONTRACT, \
+     NBO_ERC20_CONTRACT, NCT_ERC20_CONTRACT, UBO_ERC20_CONTRACT, USDC_ERC20_CONTRACT
+
+from subgrounds.plotly_wrappers import Scatter
+
+MODE_ASSET_VALUE_IN_USD = "mode_usd"
+MODE_CARBON_CUSTODIED = "mode_carbon_custodied"
+
+class TreasuryAssetWrapper:
+    def __init__(self, mode):
+        self.mode = mode
+        treasuryAssetData = []
+        treasuryAssetData.append(SingleAssetData(
+            name="BCT", address=BCT_ERC20_CONTRACT, color="#eff542", custodiesCarbon=True))
+        treasuryAssetData.append(SingleAssetData(
+            "KLIMA BCT LP", KLIMA_BCT_PAIR, "#f1c232", True))
+        treasuryAssetData.append(SingleAssetData(
+            "BCT USDC LP", BCT_USDC_PAIR, "#bf9000", False))
+        treasuryAssetData.append(SingleAssetData(
+            "MCO2", MCO2_ERC20_CONTRACT, "#93c47d", True))
+        treasuryAssetData.append(SingleAssetData(
+            "KLIMA MCO2 LP", KLIMA_MCO2_PAIR, "#6aa84f", True))
+        treasuryAssetData.append(SingleAssetData(
+            "USDC", USDC_ERC20_CONTRACT, "#e06666", False))
+        treasuryAssetData.append(SingleAssetData(
+            "KLIMA USDC LP", KLIMA_USDC_PAIR, "#cc0000", False))
+        treasuryAssetData.append(SingleAssetData(
+            "UBO", UBO_ERC20_CONTRACT, "#c27ba0", True))
+        treasuryAssetData.append(SingleAssetData(
+            "NBO", NBO_ERC20_CONTRACT, "#8e7cc3", True))
+        treasuryAssetData.append(SingleAssetData(
+            "NCT", NCT_ERC20_CONTRACT, "#6fa8dc", True))
+
+        self.asset_data = treasuryAssetData
+
+    def create_scatters(self):
+        scatters = []
+        for single_asset_data in self.asset_data:
+            if self.mode == MODE_ASSET_VALUE_IN_USD:
+                scatter = self.create_scatter(single_asset_data, single_asset_data.formatted_market_value)
+                scatters.append(scatter)
+            elif self.mode == MODE_CARBON_CUSTODIED:
+                if single_asset_data.custodiesCarbon:
+                    scatter = self.create_scatter(single_asset_data, single_asset_data.carbon_custodied)
+                    scatters.append(scatter)
+                    
+                
+        return scatters
+    
+    def create_scatter(self, single_asset_data, y_data):
+        return Scatter(
+                name=single_asset_data.name,
+                x=single_asset_data.datetime,
+                y=y_data,
+                mode='lines',
+                line={'width': 0.5, 'color': single_asset_data.color},
+                stackgroup='one',
+            )
+
+
+class SingleAssetData:
+    def __init__(self, name, address, color, custodiesCarbon):
+        self.name = name
+        self.address = address
+        self.color = color
+        self.custodiesCarbon = custodiesCarbon
+
+        #Scatter requires at least one item that is not a dataframe, seems to be a bug
+        if name == 'BCT':
+            bct_treasury_asset = get_bct_treasury()
+            self.datetime= bct_treasury_asset.datetime
+            self.formatted_market_value= bct_treasury_asset.formatted_market_value
+            self.carbon_custodied= bct_treasury_asset.carbonCustodied
+        else:
+            dataframe = get_df_for_treasury_asset_by_address(address)
+            self.datetime = dataframe.treasuryAssets_datetime
+            self.formatted_market_value = dataframe.treasuryAssets_formatted_market_value
+            self.carbon_custodied= dataframe.treasuryAssets_carbonCustodied
+        print(self.name +" is initialized")
+
